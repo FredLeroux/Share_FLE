d7779a2fa3e16c9560e5b8592197c61e92872a08
Frederic Leroux <f.leroux2903@gmail.com>
2018-06-19 00:51:45 +0200

    add main class

---
+++ P3/SourceCode/P3MasterMindFle/src/p3/MasterMind/MmMain.java
@@ -0,0 +1,14 @@
+package p3.MasterMind;
+
+public class MmMain {
+
+	public MmMain() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+new MmMenuConstruct();
+	}
+
+}
